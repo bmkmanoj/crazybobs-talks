268d47e4fcd5b581e35767df86462125a4885237
burke.eric@gmail.com <burke.eric@gmail.com@4b2c9a62-403e-11de-9a54-b9394989f659>
2010-10-12 18:53:30 +0000

    started breaking down into sections

--- src/org/crazybob/talks/androidsquared/AndroidSquared.java
+++ src/org/crazybob/talks/androidsquared/AndroidSquared.java
@@ -1,7 +1,11 @@
 // Copyright 2010 Square, Inc.
 package org.crazybob.talks.androidsquared;
 
+import org.crazybob.deck.Box;
 import org.crazybob.deck.Deck;
+import org.crazybob.deck.Slide;
+import org.crazybob.deck.Template;
+import org.crazybob.deck.Text;
 import org.crazybob.deck.templates.Square;
 
 /**
@@ -19,8 +23,42 @@ public class AndroidSquared {
         .author("Bob Lee & Eric Burke")
         .company("Square, Inc.");
 
-    
+    Template template = new Square();
 
-    deck.writePdf(new Square(), "out/androidsquared.pdf", true);
+    // Break it down into main sections for ease of collaboration.
+    squarewave(template, deck);
+    shakeSensor(template, deck);
+    retrofit(template, deck);
+
+    deck.writePdf(template, "out/androidsquared.pdf", true);
+  }
+
+  private static void squarewave(Template template, Deck deck) {
+    deck.add(sectionTitleSlide(template, "Swipe Decoding"));
+
+    // TODO
+  }
+
+  private static void shakeSensor(Template template, Deck deck) {
+    deck.add(sectionTitleSlide(template, "Shake Detection"));
+    // TODO
+  }
+
+  private static void retrofit(Template template, Deck deck) {
+    deck.add(sectionTitleSlide(template, "Retrofit"));
+
+    // TODO
+  }
+
+  private static Slide sectionTitleSlide(Template template, String title) {
+    Slide slide = new Slide();
+    Box titleBox = new Box(300, 300, 50, 50);
+    titleBox.add(new Text(title).font(template.titleFont().scale(125)));
+
+    // TODO a horizontal line would look good here
+
+    slide.add(titleBox);
+
+    return slide;
   }
 }
